6433f27d47bb6d33c63a408730054b88a56e2840
Bartosz Fabiański <bartosz.fabianski@outlook.com>
2018-12-25 13:07:03 +0100

    Map i WeakMap

--- play.js
+++ play.js
@@ -1,9 +1,47 @@
-const favoriteFood = 'gelato';
-const iLoveFood = 'The year is ' + (new Date().getFullYear()) +' and my favorite food is '+ favoriteFood;
+//Map, WeakMap, Set
 
-console.log(iLoveFood);
+//let foods = {};
+//foods.italian = 'gelato';
+//foods.mexican = 'torta';
+//foods.canadian = 'poutine';
+//
+//console.log(foods.italian);
 
-//Template template strings.
-//Zamiast cudzysłowia używamy ticka (``)
-const iLoveFood2 = `The year is ${(new Date().getFullYear())} and my favorite food is ${favoriteFood}`;
-console.log(iLoveFood2);
+//Mapa to inny sposób na tworzenie tablic asocjacyjnych
+//Warto ich użwać bo mają przydatne metody
+//w mapach można używać KLUCZY KTÓRE NIE SĄ STRINGAMI (nawet mogą być obiektami)
+
+//let foods = new Map();
+//foods.set('italian', 'gelato');
+//foods.set('mexican', 'torta');
+//foods.set('canadian', 'poutine');
+//
+//let southernUSStates = ['Tennessee', 'Kentucky', 'Texas'];
+//foods.set(southernUSStates, 'hot chicken');
+//console.log(
+//  foods.get('italian'),
+//  foods.has('french'),
+//  foods.get(southernUSStates),
+//  foods.size
+//);
+
+//WeakMap vs Map - klucze WeakMap MUSZĄ być obiektami
+//Zamieniam klucze na tablice, bo tablice są obiektami w JS
+// set(['italian'] i get(['italian'] nie są takimi samymi obiektami w pamięci, są różnymi obiektami
+//w WeakMap nie można używać .size
+
+let foods = new WeakMap();
+foods.set(['italian'], 'gelato');
+foods.set(['mexican'], 'torta');
+foods.set(['canadian'], 'poutine');
+
+let southernUSStates = ['Tennessee', 'Kentucky', 'Texas'];
+foods.set(southernUSStates, 'hot chicken');
+southernUSStates = null; //Przypisanie WeakMap do null pozwala garbage collectorowi na jej usuniącie, odwrotnie jest w Map
+//ale lepiej używać Map
+
+console.log(
+  foods.get(['italian']),
+  foods.get(southernUSStates),
+  foods.size
+);
