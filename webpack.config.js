b282059abfcfac789afabbfbf828bd05d360fde9
Bartosz Fabiański <bartosz.fabianski@outlook.com>
2018-12-25 20:04:16 +0100

    Dodanie wbpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,10 @@
+//Bez ./ require szuka corowej biblioteki w node_modules albo Webpacku
+const path = require('path');
+
+module.exports = {
+  entry: './web/assets/js/RepLogApp.js',
+  output: {
+    path: path.resolve(__dirname, 'web', 'build'),
+    filename: 'rep_log.js',
+  }
+};
